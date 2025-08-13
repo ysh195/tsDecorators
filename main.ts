767ffbae256551b39f7ab1f7b81f00cb362ba7f1
ysh195 <159318873+ysh195@users.noreply.github.com>
2025-08-13 14:43:37 +0900

    Update main.ts

--- main.ts
+++ main.ts
@@ -15,14 +15,15 @@ import bootstrap, {
     Validate,
     ValidatedWith
 } from "./ysh-decorator";
+// 직접 생성한 컨트롤러 클래스나 주입이 필요한 클래스를 임포트합니다.
 
 const port = 3000;
 
 const app = express();
 app.use(express.json());
 
-const controllers: any[] = [];
-const others: any[] = [];
+const controllers: any[] = []; // 컨트롤러 클래스 자체를 여기에 추가합니다.
+const others: any[] = []; // 그 외 주입이 필요한 클래스 자체를 여기에 추가합니다.
 
 bootstrap(app, controllers, others);
 
